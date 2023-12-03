b67e8ce6fdced4c9413dda7a8a691808b50268fb
Landon Lee <lturtlesrock@gmail.com>
2023-12-02 20:47:36 -0600

    fixed no schema, and no export

--- routes/toys.js
+++ routes/toys.js
@@ -2,8 +2,13 @@ const app = require('express');
 const router = app.Router();
 const db = require("../models/index");
 
-const controller = require("../controllers/toys");
-const {isAuthenticated} = require('../middleware/authenticate');
+const controller = require("../controllers/toys")(db.toys);
+//const {isAuthenticated} = require('../middleware/authenticate');
 
 router.get("/", controller.getAll);
 router.get("/:id", controller.getSingle);
+router.post("/", controller.create);
+router.put("/:id", controller.update);
+router.delete("/:id", controller.deleteEntry);
+
+module.exports = router;
