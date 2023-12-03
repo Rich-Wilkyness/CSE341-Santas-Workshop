42bd792f1fa2883e6fc9248b481affbd6a177f6b
Rich-Wilkyness <r.christian.wilkerson@gmail.com>
2023-12-02 20:47:12 -0700

    trying to fix swagger issues

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'Elf User Information',
     },
     host: 'https://cse341-santas-workshop.onrender.com',
-    schemes: ['http'],
+    schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
